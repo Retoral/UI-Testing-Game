239cb0cea7fbbd59f2a83cc520ece8ac9da0477d
Marry Nightmare <tayuu@hotmail.de>
2017-05-26 23:46:21 +0200

    [FIX] removed old Resource.cs and IncResource.cs, adjusted TimerBar.cs and added UpdateIncomeLabel.cs
    Signed-off-by: Marry Nightmare <tayuu@hotmail.de>

--- UI Game/Assets/TimerBar.cs
+++ UI Game/Assets/TimerBar.cs
@@ -22,7 +22,7 @@ public class TimerBar : MonoBehaviour {
         if (Durration > 0)
         {
             float timer = Time.deltaTime;
-            Durration -= timer * ( 1+ (1/2) * IncResource.GlobalResource.Builders);
+            Durration -= timer * ( 1+ (1/2) * MNCreateResource.GlobalResources[ListOfResources.Builder].CurrentAmount);
             ProgressBar.fillAmount = Durration / MaxDurration;
             DisplayText.text = Durration.ToString("0") + "Sek";
         } if (Durration < 0)
@@ -31,30 +31,4 @@ public class TimerBar : MonoBehaviour {
 
         }
     }
-
-
-    //public void Start()
-    //{
-    //    Durration = MaxDurration;
-
-    //    UpdateBar();
-    //}
-
-    //private void UpdateBar()
-    //{
-    //    float ratio = Durration / MaxDurration;
-    //    ProgressBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
-    //    RatioText.text = (Durration).ToString();
-    //}
-
-    //private void ReductionTime(float timer)
-    //{
-    //    timer = Time.deltaTime;
-    //    Durration -= timer;
-    //    if (Durration < 0)
-    //    {
-    //        Durration = 0;
-    //        Debug.Log("It's done!");
-    //    }
-    //}
 }
---
+++ UI Game/Assets/UpdateIncomeLabel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateIncomeLabel : MonoBehaviour
+{
+
+  [Header("On = Total, Off = Per Min")]
+  public bool TotalOrPerMin;
+  [Space(10)]
+  public Text incResource;
+  public ListOfResources resource;
+
+  private void Awake()
+  {
+    SetRessourceAutomaticly();
+  }
+
+
+  private void SetRessourceAutomaticly()
+  {
+    if (incResource == null)
+    {
+      incResource = transform.GetChild(0).GetComponent<Text>();
+    }
+    try
+    {
+      resource = (ListOfResources)Enum.Parse(typeof(ListOfResources), transform.name, true);
+    }
+    catch (ArgumentException)
+    {
+      Debug.LogError("Parent has name (" + transform.name +") that does not match with ListOfResources");
+      resource = ListOfResources.Error;
+    }
+  }
+
+
+  void Update()
+  {
+    if (MNCreateResource.GlobalResources.ContainsKey(resource))
+    {
+      incResource.text = TotalOrPerMin ? MNCreateResource.GlobalResources[resource].CurrentAmount.ToString()
+                                       : MNCreateResource.GlobalResources[resource].IncomePerMinute.ToString();
+    }
+  }
+}
